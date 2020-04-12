e506bc58e26cfd9843defdf454a1585eb18c5d1c
Oday Mansour <odaym@nsour.net>
2020-04-12 01:27:11 +0100

    Cleaned up fsm code

--- fsm.py
+++ fsm.py
@@ -1,23 +1,23 @@
 # Finite state machine code
 
-def say_hello():
-    print("Hello!")
-
-
+###############################################
+# State machine code ##########################
 class State:
     name = ""
     description = ""
 
-    def __init__(self, name):
+    def __init__(self, name, description):
         self.name = name
+        self.description = description
 
 
 class Event:
     name = ""
     description = ""
 
-    def __init__(self, name):
+    def __init__(self, name, description):
         self.name = name
+        self.description = description
 
 
 class Action:
@@ -25,8 +25,9 @@ class Action:
     description = ""
     callback = None
 
-    def __init__(self, name, callback):
+    def __init__(self, name, description, callback):
         self.name = name
+        self.description = description
         self.callback = callback
 
     def run(self):
@@ -41,8 +42,9 @@ class Transition:
     out_action = None
     out_state = None
 
-    def __init__(self, name, in_state, in_event, out_action, out_state):
+    def __init__(self, name, description, in_state, in_event, out_action, out_state):
         self.name = name
+        self.description = description
         self.in_state = in_state
         self.in_event = in_event
         self.out_action = out_action
@@ -50,52 +52,87 @@ class Transition:
 
 
 class FSM:
+    name = ""
+    description = ""
     states = []
     events = []
     actions = []
     transitions = []
     current_state = None
 
-    def __init__(self):
-        self.states.append(State("summary"))
-        self.states.append(State("helloing"))
-        self.events.append(Event("click"))
-        self.actions.append(Action("say_hello", say_hello))
-        self.transitions.append(Transition("summary_to_hello", self.states[0], self.events[0], self.actions[0], self.states[1]))
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
 
-        self.current_state = self.states[0]
+    def addState(self, state, start_state=False):
+        self.states.append(state)
+        if start_state:
+            self.current_state = state
 
+    def addEvent(self, event):
+        self.events.append(event)
+
+    def addAction(self, action):
+        self.actions.append(action)
+
+    def addTransition(self, transition):
+        self.transitions.append(transition)
 
     def transition(self, event_name):
-        event = Event("")
-        transition = Transition("", None, None, None, None)
-
-        print("Looking for event " + event_name)
+        print("Looking for event " + event_name + "... ", end = '')
         event_list = list(filter(lambda event: event.name == event_name, self.events))
         if len(event_list) == 1:
             event = event_list[0]
-            print("Found event " + event.name)
+            print("found event " + event.name)
         else:
-            print("Did not find one event " + event_name)
+            print("did not find one event " + event_name)
             return False
         
-        print("Currently at state " + self.current_state.name)
-        print("Looking for transaction with inputs " + self.current_state.name + " + " + event.name)
+        print("Looking for transaction with inputs " + self.current_state.name + " + " + event.name + "... ", end = '')
         transition_list = list(filter(lambda transition: transition.in_state == self.current_state and transition.in_event == event, self.transitions))
-
         if len(transition_list) == 1:
             transition = transition_list[0]
-            print("Found transition " + transition.name)
+            print("found transition " + transition.name)
         else:
-            print("Did not find transition with inputs " + self.current_state + " + " + event.name)
+            print("fid not find transition with inputs " + self.current_state + " + " + event.name)
             return False
 
-        print("Running action and moving to new state:")
+        print("Running action and moving to new state: " + transition.out_state.name)
         transition.out_action.run()
         self.current_state = transition.out_state
         
-        print("Now at state " + self.current_state.name)
+
+###############################################
+# State machine configuration #################
+
+# Initialise ##############
+fsm = FSM("fsm", "Main interaction and display state machine")
+
+# Callbacks ###############
+def say_hello():
+    print("Hello!")
+
+# States ##################
+s_summary = State("summary", "Summary page")
+fsm.addState(s_summary, start_state=True)
+
+s_hellowing = State("hellowing", "Page after saying hello")
+fsm.addState(s_hellowing)
+
+# Events ##################
+e_click = Event("click", "Click event")
+fsm.addEvent(e_click)
+
+# Actions #################
+a_sayhello = Action("say_hello", "Say hello", say_hello)
+fsm.addAction(a_sayhello)
+
+# Transitions #############
+t_summary_click = Transition("summary_click", "Click while on Summary", s_summary, e_click, a_sayhello, s_hellowing)
+fsm.addTransition(t_summary_click)
 
 
-fsm = FSM()
+###############################################
+# Section to play around ######################
+
 fsm.transition("click")
