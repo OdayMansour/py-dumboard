53d31446e471f8137f5d2c0af7fd3e889989c853
Oday Mansour <odaym@nsour.net>
2020-04-13 00:18:46 +0100

    Defined, visualized, and coded in the board's FSM (only one display state)

--- fsm.py
+++ fsm.py
@@ -68,15 +68,19 @@ class FSM:
         self.states.append(state)
         if start_state:
             self.current_state = state
+        return state
 
     def addEvent(self, event):
         self.events.append(event)
+        return event
 
     def addAction(self, action):
         self.actions.append(action)
+        return action
 
     def addTransition(self, transition):
         self.transitions.append(transition)
+        return transition
 
     def transition(self, event_name):
         print("Looking for event " + event_name + "... ", end = '')
@@ -97,7 +101,7 @@ class FSM:
             print("fid not find transition with inputs " + self.current_state + " + " + event.name)
             return False
 
-        print("Running action and moving to new state: " + transition.out_state.name)
+        print("Running action " + transition.out_action.name + " and moving to state " + transition.out_state.name)
         transition.out_action.run()
         self.current_state = transition.out_state
         
@@ -113,24 +117,45 @@ def say_hello():
     print("Hello!")
 
 # States ##################
-s_summary = State("summary", "Summary page")
-fsm.addState(s_summary, start_state=True)
-
-s_hellowing = State("hellowing", "Page after saying hello")
-fsm.addState(s_hellowing)
+s_sleep = fsm.addState(State("sleep", "Sleeping, lights off"), start_state=True)
+s_summary = fsm.addState(State("summary", "Summary page"))
+s_selection = fsm.addState(State("selection", "Selection"))
+s_display = fsm.addState(State("display", "Displaying daily temperature info"))
 
 # Events ##################
-e_click = Event("click", "Click event")
-fsm.addEvent(e_click)
+e_click = fsm.addEvent(Event("click", "Click event"))
+e_rot_pos = fsm.addEvent(Event("rorate_pos", "Rotating knob, positive"))
+e_rot_neg = fsm.addEvent(Event("rotate_neg", "Rotating knob, positive"))
+e_timer = fsm.addEvent(Event("timer", "Sleep timer expiring"))
 
 # Actions #################
-a_sayhello = Action("say_hello", "Say hello", say_hello)
-fsm.addAction(a_sayhello)
+a_show_summary = fsm.addAction(Action("show_summary", "Display summary page", say_hello))
+a_show_selection = fsm.addAction(Action("show_selection", "Display selection page", say_hello))
+a_show_display = fsm.addAction(Action("show_display", "Display temperature info page", say_hello))
+a_pos_selection = fsm.addAction(Action("pos_selection", "Increase selection counter", say_hello))
+a_neg_selection = fsm.addAction(Action("neg_selection", "Decrease selection counter", say_hello))
+a_goto_sleep = fsm.addAction(Action("goto_sleep", "Go to sleep", say_hello))
 
 # Transitions #############
-t_summary_click = Transition("summary_click", "Click while on Summary", s_summary, e_click, a_sayhello, s_hellowing)
-fsm.addTransition(t_summary_click)
+t_sleep_click = fsm.addTransition(Transition("sleep_click", "Click while sleeping", s_sleep, e_click, a_show_summary, s_summary))
+t_sleep_rot_pos = fsm.addTransition(Transition("sleep_rot_pos", "Rotate positive while sleeping", s_sleep, e_rot_pos, a_show_selection, s_selection))
+t_sleep_rot_neg = fsm.addTransition(Transition("sleep_rot_neg", "Rotate negative while sleeping", s_sleep, e_rot_neg, a_show_selection, s_selection))
+t_sleep_timer = fsm.addTransition(Transition("sleep_timer", "Timer runs out while sleeping", s_sleep, e_timer, a_show_selection, s_sleep))
 
+t_summary_click = fsm.addTransition(Transition("summary_click", "Click while on summary", s_summary, e_click, a_goto_sleep, s_sleep))
+t_summary_rot_pos = fsm.addTransition(Transition("summary_rot_pos", "Rotate positive while on summary", s_summary, e_rot_pos, a_show_selection, s_selection))
+t_summary_rot_neg = fsm.addTransition(Transition("summary_rot_neg", "Rotate negative while on summary", s_summary, e_rot_neg, a_show_selection, s_selection))
+t_summary_timer = fsm.addTransition(Transition("summary_timer", "Timer runs out while on summary", s_summary, e_timer, a_goto_sleep, s_sleep))
+
+t_selection_click = fsm.addTransition(Transition("selection_click", "Click while on selection", s_selection, e_click, a_show_display, s_display))
+t_selection_rot_pos = fsm.addTransition(Transition("selection_rot_pos", "Rotate positive while on selection", s_selection, e_rot_pos, a_pos_selection, s_selection))
+t_selection_rot_neg = fsm.addTransition(Transition("selection_rot_neg", "Rotate negative while on selection", s_selection, e_rot_neg, a_neg_selection, s_selection))
+t_selection_timer = fsm.addTransition(Transition("selection_timer", "Timer runs out while on selection", s_selection, e_timer, a_show_summary, s_summary))
+
+t_display_click = fsm.addTransition(Transition("display_click", "", s_display, e_click, a_show_summary, s_summary))
+t_display_rot_pos = fsm.addTransition(Transition("display_rot_pos", "", s_display, e_rot_pos, a_show_selection, s_selection))
+t_display_rot_neg = fsm.addTransition(Transition("display_rot_neg", "", s_display, e_rot_neg, a_show_selection, s_selection))
+t_display_timer = fsm.addTransition(Transition("display_timer", "", s_display, e_timer, a_show_summary, s_summary))
 
 ###############################################
 # Section to play around ######################
